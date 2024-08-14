f336b0a7b7f16f218e03b448403e972db2e4fd1d
yunseohhe <dbstj4566@naver.com>
2024-08-14 12:47:06 +0900

    초기세팅

---
+++ src/main/java/com/sparta/spring_init_homework/SpringInitHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_init_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/spring_init_homework/SpringInitHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_init_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
